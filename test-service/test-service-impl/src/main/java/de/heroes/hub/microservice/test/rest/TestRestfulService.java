983c555d9cbd913226cbafe17f84dbc13486831f
DerMajo <bad.backe@gmx.de>
2019-09-12 06:51:21 +0000

    refactoring

--- test-service/test-service-impl/src/main/java/de/heroes/hub/microservice/test/rest/TestRestfulService.java
+++ test-service/test-service-impl/src/main/java/de/heroes/hub/microservice/test/rest/TestRestfulService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.heroes.hub.microservice.test.rest.item.GreetingResponseItem;
+import de.heroes.hub.microservice.test.response.GreetingResponseItem;
 
 import java.util.concurrent.atomic.AtomicLong;
 
